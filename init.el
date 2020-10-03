04c60769078424be7f309215a26a361abb0f0504
nuxion <nuxion@gmail.com>
2020-10-03 19:09:31 -0300

    go support

--- init.el
+++ init.el
@@ -1,12 +1,42 @@
-;; Xavi's config
+;:; init.el --- Summary
 
-;; general configs
-(defconst *spell-check-support-enabled* t) ;; Enable with t if you prefer
+;; inspirations
+;; 
+
+;;; Code:
+(defconst *spell-check-support-enabled* t)
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode))
 
+;; paths
+(setenv "PATH"
+  (concat (getenv "PATH")
+   ":/home/nuxion/.local/bin:/home/nuxion/go/bin:/usr/local/bin:/bin:/usr/bin"
+  )
+)
+;; general settings
+(use-package xclip
+  :demand t
+  :config
+  (progn
+    (xclip-mode 1)))
+
+;; increase the number of message in the buffer
+(setq message-log-max 10000)
+
+;; garbage collection threshold
+(setq gc-cons-threshold 100000000
+      garbage-collection-messages t)
+
 ;; no startup message
 (setq inhibit-startup-message t)
+
+
+;; disable backup
+(setq backup-inhibited t)
+;; disable auto save
+(setq auto-save-default nil)
+
 
 ;; Package configs
 (require 'package)
@@ -22,7 +52,7 @@
 ;; Next, we'll use use-package to configure our packages better,
 ;; in case you don't know, this package provides a macro to allow
 ;; you to easily install packages and isolate package configuration
-;; in a way that is both performance-oriented and tidy. 
+;; in a way that is both performance-oriented and tidy.
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -42,7 +72,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(projectile flycheck lsp-ui which-key magit doom-themes use-package)))
+   '(ivy-rich counsel go-mode company-lsp company projectile flycheck lsp-ui which-key magit doom-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -66,10 +96,121 @@
   :config
   (which-key-mode))
 
+;;(add-to-list 'load-path "/home/nuxion/emacs.d/evil")
+(use-package evil
+  :ensure t
+)
+(require 'evil)
+(evil-mode 1)
+
+(package-install 'flycheck)
+(global-flycheck-mode)
+
+;; Projectile configuration
 (use-package projectile
   :ensure t
   :pin melpa-stable
   :config
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-  (projectile-mode +1))
+  (projectile-mode +1)
+  projectile-project-search-path '("~/Proyects" "~/Proto/")
+  )
+
+;(setq projectile-project-search-path '("~/Proyects" "~/Proto/"))
+
+
+
+;; language server
+(use-package go-mode
+  :mode "\\.go$"
+  :ensure t
+  )
+
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (
+	 (go-mode . lsp)
+	 (python-mode . lsp)
+	 )
+  )
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :init
+ )
+
+(use-package counsel
+  :ensure t
+  :after ivy
+  :config (counsel-mode))
+
+(use-package ivy
+  :ensure t
+  :defer 0.1
+  :diminish
+  :bind (("C-c C-r" . ivy-resume)
+         ("C-x B" . ivy-switch-buffer-other-window))
+  :custom
+  (ivy-count-format "(%d/%d) ")
+  (ivy-use-virtual-buffers t)
+  :config (ivy-mode)
+  )
+
+(use-package ivy-rich
+  :ensure t
+  :after ivy
+  :custom
+  (ivy-virtual-abbreviate 'full
+                          ivy-rich-switch-buffer-align-virtual-buffer t
+                          ivy-rich-path-style 'abbrev)
+  :config
+  (ivy-set-display-transformer 'ivy-switch-buffer
+                               'ivy-rich-switch-buffer-transformer))
+
+(use-package swiper
+  :ensure t
+  :after ivy
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
+
+
+;;Company mode is a standard completion package that works well with lsp-mode.
+;;company-lsp integrates company mode completion with lsp-mode.
+;;completion-at-point also works out of the box but doesn't support snippets.
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+  :hook ((emacs-lisp-mode . company-mode)
+         (python-mode . company-mode)
+         (go-mode . company-mode)
+         (typescript-mode . company-mode)
+         (restclient-mode . company-mode)
+         (js-mode . company-mode)))
+
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp)
+
+
+
+;; Go lang config
+;; based on
+;; https://arenzana.org/2019/12/emacs-go-mode-revisited/
+;(setq lsp-gopls-staticcheck t)
+;(setq lsp-eldoc-render-all t)
+;(setq lsp-gopls-complete-unimported t)
+
+
+;;Set up before-save hooks to format buffer and add/delete imports.
+;;Make sure you don't have other gofmt/goimports hooks enabled.
+;(defun lsp-go-install-save-hooks ()
+;  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+;  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+					;(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+;;; init.el ends here
